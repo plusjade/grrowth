ec8499b1393ef86bcf5e84bbcabc01ca2506bb5b
plusjade <plusjade@gmail.com>
2010-07-04 04:56:28 -0700

    setup simple cache rendering for displaying live pages via 'deploy' controller

--- app/controllers/deploy_controller.rb
+++ app/controllers/deploy_controller.rb
@@ -6,16 +6,38 @@ class DeployController < ApplicationController
   def index
     #render :text => 'Facebook credentials not sent.' and return if params['fb_sig_page_id'].nil?
     @id = '131864086829765'
-    
-    # TODO: look for a cached page before hitting the db.
-    
-    @page = Page.first(:conditions => { :fb_sig_page_id => @id })
-    render :template => 'deploy/generic.erb' and return if @page.nil? 
-    
-    
-    @jade = {'type'=> 'human'}
-
+    @path = File.join('tmp/cache', "#{@id}.html")
+    render :text => render_cache
+    return 
   end
 
+  
+  private
+  def render_cache
+    update_cache if !File.exist?(@path)
+    
+    f = File.open(@path) 
+    return f.read
+  end
+  
+  
+  def update_cache
+    f = File.new(@path, "w+")
+    f.write(build_page)
+    f.rewind    
+  end
+  
 
+  def build_page
+    @page = Page.first(:conditions => { :fb_sig_page_id => @id })
+    
+    if @page.nil?
+      return render_to_string(:template => "deploy/generic.erb")  
+    else
+      return render_to_string(:template => "deploy/index.erb") 
+    end
+  end  
+
+  
+   
 end
