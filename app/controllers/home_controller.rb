cba3c793b8d606c33b8a5ce3f076dd08b0a0e280
plusjade <plusjade@gmail.com>
2010-07-13 06:21:55 -0700

    super basic homepage update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,11 @@ class HomeController < ApplicationController
   skip_before_filter :verify_authenticity_token
   
   def index
-    render :text => 'Some FBML ehboy!'
-    return
+    if request.post?
+      render :text => 'Some FBML ehboy!'
+      return
+    end
+    
   end
   
 end
