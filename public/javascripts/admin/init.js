927fd9fb951e0cc5761c7e6f8f16533b8ded5063
plusjade <plusjade@gmail.com>
2010-07-13 21:16:26 -0700

    refactored javascript

--- public/javascripts/admin/init.js
+++ public/javascripts/admin/init.js
@@ -1,28 +1,30 @@
 ;$(document).ready(function(){
-  loading = '<div class="loading">Loading...</div>';
-  function addNew(index){ return '<li><div class="info"><span>Slide '+ index +' </span><a href="#" class="remove">[x]</a><a href="#" class="edit">edit</a></div><div class="edit"><textarea id="slider_slides_'+ index +' " name="slider_slides['+ index +']">slide number: '+ index +'</textarea></div></li>';};
+  var loading = '<div class="loading">Loading...</div>';
+  function newSlide(index){ return '<li><div class="info"><span>Slide '+ index +' </span><a href="#" class="remove">[x]</a><a href="#" class="edit">edit</a></div><div class="edit"><textarea id="slider_slides_'+ index +' " name="slider_slides['+ index +']">slide number: '+ index +'</textarea></div></li>';};
   function newSlider(s) { return '<option value="/sliders/'+s.id+'/edit">'+s.name+' => <b>[#slider:'+s.id+']</b></option>'};
 
 // update sliders list.
   function updateSliders(add){
     $.getJSON('/pages/'+$('#main').attr('rel')+'/sliders.json', function(rsp){
-      var links = '<option value="0">Select a Slideshow</option>';
+      var $sliders = $('#sliders-list'),
+        links = '<option value="0">Select a Slideshow</option>';
       $.each(rsp, function(){
         links += newSlider(this.slider);
       });
-      $('select.widget-list').html(links);
+      $sliders.html(links);
       if(add){
-        $("select.widget-list option:selected").removeAttr("selected");
-        $("select.widget-list option:last").attr("selected", "selected");
+        $("option:selected", $sliders).removeAttr("selected");
+        $("option:last", $sliders).attr("selected", "selected");
       }
     })  
   }             
   function loadInto(url, div, callback){
-    $(div).html(loading);
+    var $div = $(div);
+    $div.html(loading);
     $.get(url, function(view){
-      $(div).html(view);
-      $(document).trigger('ajaxify.forms');
+      $div.html(view);
       if(callback && (typeof callback == 'function')) callback();
+      $(document).trigger('ajaxify.forms');
     })    
   }
   
@@ -37,20 +39,20 @@
       return false;
     },
     // load pages into the editing environment
-    'ul#pages-list li a.edit' : function(e){
+    '#pages-list li a.edit' : function(e){
       $.facebox.close();
       loadInto(e.target.href, '#holder', function(){
-        $('ul#page-tabs li a:first').click();
+        $(document).trigger('page.init');
       })
       return false;
     },
   
    // activate tab navigation for page editing panel
-    'ul#page-tabs li a' : function(e){
+    '#page-tabs li a' : function(e){
       $('div.tab-content').hide();
-      $('ul#page-tabs li a').removeClass('active');
+      $('#page-tabs li a').removeClass('active');
       $(e.target).addClass('active');
-      var $tab = $('div#'+ $(e.target).attr('rel'));
+      var $tab = $('#'+ $(e.target).attr('rel'));
       $tab.show();
       if(e.target.id == 'load'){
         $tab.html(loading);
@@ -59,16 +61,16 @@
         })      
       }
       // this is so javascript in the preview gets removed.
-      $('div#tab-view').empty();
+      $('#tab-view').empty();
       return false;
     },
     
    // activate tab navigation for widget editing panel
-    'ul#widget-tabs li a' : function(e){
+    '#widget-tabs li a' : function(e){
       $('div.widget-tabs').hide();
-      $('ul#widget-tabs li a').removeClass('active');
+      $('#widget-tabs li a').removeClass('active');
       $(e.target).addClass('active');
-      var $tab = $('div#'+ $(e.target).attr('rel'));
+      var $tab = $('#'+ $(e.target).attr('rel'));
       $tab.show();
       if(e.target.id == 'loadachu'){
         $tab.html(loading);
@@ -78,15 +80,7 @@
       }
       return false;
     },
-    
-   // load widgets editable environment
-    'ul.widget-list li a.edit' : function(e){
-      loadInto(e.target.href, 'div#widget-wrapper', function(){
-         $('ul#widget-tabs li a:first').click();
-      });
-      return false;
-    },
-        
+            
     // delete a resource using REST.
     'a.delete' : function(e){
       $.ajax({
@@ -104,7 +98,7 @@
             $('#holder').empty();
           }
           if($(e.target).attr('rel') == 'sliders'){
-            $('div#widget-wrapper').empty();
+            $('#widget-wrapper').empty();
             updateSliders();
           }
         }
@@ -115,21 +109,21 @@
   edit sliders environment
 */ 
    // overload save button to change textarea insertion order.
-    'button#save-slider' : function(e){
-      $.each($('ul#slides-list li'), function(key, node){
+    '#save-slider' : function(e){
+      $.each($('#slides-list li'), function(key, node){
         $('textarea', $(this)).attr('name', 'slider_slides['+ key +']');
       })
     },
-    'a#new-slide' : function(e){
-      var index = $('ul#slides-list li').length;
-      $('ul#slides-list').append(addNew(index));
+    '#new-slide' : function(e){
+      var index = $('#slides-list li').length;
+      $('#slides-list').append(newSlide(index));
       return false;
     },
-    'ul#slides-list a.edit' :function(e){
+    '#slides-list a.edit' :function(e){
       $(e.target).parent().next('div').toggle();
       return false;
     },  
-    'ul#slides-list a.remove' :function(e){
+    '#slides-list a.remove' :function(e){
       $(e.target).parent().parent('li').remove();
       return false;
     }   
@@ -152,13 +146,13 @@
           if(rsp.created.resource == 'pages'){
             $.facebox.close();
             loadInto('/pages/'+ rsp.created.id +'/edit', '#holder', function(){
-              $('ul#page-tabs li a:first').click();
+              $('#page-tabs li a:first').click();
             })         
           }  
           if(rsp.created.resource == 'sliders'){
             $.facebox.close();
-            loadInto('/sliders/'+ rsp.created.id +'/edit', 'div#widget-wrapper', function(){
-               $('ul#widget-tabs li a:first').click();
+            loadInto('/sliders/'+ rsp.created.id +'/edit', '#widget-wrapper', function(){
+               $('#widget-tabs li a:first').click();
             });
             updateSliders(true);
           }  
@@ -169,7 +163,98 @@
       }
     });
   });
+
+ // initialize the page edit environment
+  $(document).bind('page.init', function(){
   
+    $('#page-tabs li a:first').click();
+    $('select.widget-list').change(function(){
+      var url = $('option:selected', $(this)).val();
+      if(url == 0) return;
+      loadInto(url, '#widget-wrapper', function(){
+        $(document).trigger('widget.init');
+      })       
+      return false;
+    });
+    
+    var pageBody = CodeMirror.fromTextArea('page_body_helper', {
+      width: "820px",
+      height: "700px",
+      parserfile: ["parsexml.js", "parsecss.js", "tokenizejavascript.js", "parsejavascript.js", "parsehtmlmixed.js"],
+      stylesheet: ["/stylesheets/codemirror/xmlcolors.css?232331sdf", "/stylesheets/codemirror/jscolors.css?12312"],
+      path: "/javascripts/codemirror/",
+      continuousScanning: 500,
+      lineNumbers: true,
+      textWrapping: false,
+      saveFunction: function(){
+        $('#page_body').val(pageBody.getCode());
+        $('form.edit_page').submit();
+      },
+      initCallback: function(editor){
+        editor.setCode($('#page_body').val());    
+      }    
+    });
+
+    var pageCss = CodeMirror.fromTextArea('page_css_helper', {
+      width: "820px",
+      height: "700px",
+      parserfile: "parsecss.js",
+      stylesheet: "/stylesheets/codemirror/csscolors.css?3453",
+      path: "/javascripts/codemirror/",
+      continuousScanning: 500,
+      lineNumbers: true,
+      saveFunction: function(){
+        $('#page_css').val(pageCss.getCode());
+        $('form.edit_page').submit();
+      },
+      initCallback: function(editor){
+        editor.setCode($('#page_css').val());
+      }
+    });
+
+   // overload save button for saving a page
+    $('#save-page').click(function(){
+      $('#page_body').val(pageBody.getCode());
+      $('#page_css').val(pageCss.getCode());
+    })  
+  });
+
+
+ // initialize the widget edit environment
+  $(document).bind('widget.init', function(){
+    $('#widget-tabs li a:first').click();
+    $("#slides-list").sortable({axis: 'y' });
+    
+    var sampleHtml = CodeMirror.fromTextArea('sample-html', {
+      readOnly: true,
+      width: "820px",
+      height: "700px",
+      parserfile: ["parsexml.js", "parsecss.js", "tokenizejavascript.js", "parsejavascript.js", "parsehtmlmixed.js"],
+      stylesheet: ["/stylesheets/codemirror/xmlcolors.css?232331sdf", "/stylesheets/codemirror/jscolors.css?12312"],
+      path: "/javascripts/codemirror/",
+      lineNumbers: true,
+      textWrapping: false,
+
+    });
+
+    var sliderCss = CodeMirror.fromTextArea('slider_css', {
+      width: "820px",
+      height: "700px",
+      parserfile: "parsecss.js",
+      stylesheet: "/stylesheets/codemirror/csscolors.css?3453",
+      path: "/javascripts/codemirror/",
+      continuousScanning: 500,
+      lineNumbers: true,
+      saveFunction: function(){
+        $('#slider_css').val(sliderCss.getCode());
+        $('form.edit_slider').submit();
+      }
+    })
+    
+    $(document).trigger('ajaxify.forms');
+  });
+  
+    
   // facebox reveal callback  
   $(document).bind('reveal.facebox', function(){
     $(document).trigger('ajaxify.forms');
@@ -193,6 +278,6 @@
     $('div.submitting').hide();
     $('div.responding.active').remove();
     $('div.responding').clone().addClass('active ' + status).html(msg).show().insertAfter('.responding');
-    setTimeout('$(".responding.active").fadeOut(4000)', 1900);  
+    setTimeout('$("div.responding.active").fadeOut(4000)', 1900);  
   }); 
 });
