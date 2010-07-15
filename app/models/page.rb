a0535ffe0a8111ef1667d794321802d5bad85cf6
plusjade <plusjade@gmail.com>
2010-07-15 04:44:04 -0700

    fb ids should be only numbers/letters

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,6 +3,7 @@ class Page < ActiveRecord::Base
   has_many :sliders
   validates_presence_of :name
   validates_length_of :fb_sig_page_id, :minimum => 10
+  validates_format_of :fb_sig_page_id, :with => /\A[a-zA-Z0-9]+\z/
   validates_uniqueness_of :fb_sig_page_id
   after_save :renew_cache
   
